a40d7a0ddd169142d6cf5e71a0d0f2e2e9686544
dcs-soni <52232900+dcs-soni@users.noreply.github.com>
2025-02-26 19:14:06 +0530

    feat: add SVG icons of socials

--- public/icons/X.tsx
+++ public/icons/X.tsx
@@ -1,13 +1,15 @@
-export const X = () => {
+export const XIcon = () => {
   return (
     <svg
-      className="h-6 w-6 text-zinc-400 dark:text-zinc-100"
+      className="h-4 w-4 text-zinc-400 dark:text-zinc-100"
       stroke="currentColor"
-      role="img"
-      viewBox="0 0 30 30"
+      viewBox="0 0 24 24"
       xmlns="http://www.w3.org/2000/svg">
       <title>X</title>
-      <path d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z" />
+      <path
+        fill="currentColor"
+        d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z"
+      />
     </svg>
   );
 };
