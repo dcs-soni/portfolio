f528f9025057acc496ca2d1c99553bb346c3acd6
dcs-soni <52232900+dcs-soni@users.noreply.github.com>
2025-02-28 18:48:00 +0530

    fix: add a compose icon and a single file to export all icons at once

---
+++ public/icons/index.ts
@@ -0,0 +1,6 @@
+import { GitHubIcon } from "./GitHub";
+import { LinkedInIcon } from "./LinkedIn";
+import { XIcon } from "./X";
+import { ComposeIcon } from "./Compose";
+
+export { GitHubIcon, LinkedInIcon, XIcon, ComposeIcon };
