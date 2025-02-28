efe435c64c72bf5fa14383c8c57a26cd22fbb07b
dcs-soni <52232900+dcs-soni@users.noreply.github.com>
2025-02-28 18:52:08 +0530

    fix: add a common file to export icons and fix the imports in this file

--- public/icons/index.ts
+++
@@ -1,6 +0,0 @@
-import { GitHubIcon } from "./GitHub";
-import { LinkedInIcon } from "./LinkedIn";
-import { XIcon } from "./X";
-import { ComposeIcon } from "./Compose";
-
-export { GitHubIcon, LinkedInIcon, XIcon, ComposeIcon };
---
+++ public/icons/index.tsx
@@ -0,0 +1,6 @@
+import { GitHubIcon } from "@/public/icons/GitHub";
+import { LinkedInIcon } from "@/public/icons/LinkedIn";
+import { XIcon } from "@/public/icons/X";
+import { ComposeIcon } from "@/public/icons/Compose";
+
+export { GitHubIcon, LinkedInIcon, XIcon, ComposeIcon };
