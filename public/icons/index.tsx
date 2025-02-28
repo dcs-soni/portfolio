ba5d4e75213fdd7d11b18fc27ab9233f3007b9a4
dcs-soni <52232900+dcs-soni@users.noreply.github.com>
2025-02-28 19:15:31 +0530

    fix: change folder structure of icons, moved to components

--- public/icons/index.tsx
+++
@@ -1,6 +0,0 @@
-import { GitHubIcon } from "@/public/icons/GitHubIcon";
-import { LinkedInIcon } from "@/public/icons/LinkedInIcon";
-import { XIcon } from "@/public/icons/XIcon";
-import { ComposeIcon } from "@/public/icons/ComposeIcon";
-
-export { GitHubIcon, LinkedInIcon, XIcon, ComposeIcon };
