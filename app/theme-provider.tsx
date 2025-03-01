4cca0debfe445793286676aa3beca0508f8a709a
dcs-soni <52232900+dcs-soni@users.noreply.github.com>
2025-03-01 19:13:50 +0530

    chore: add relevant attr to improve theme and fix layout

--- app/theme-provider.tsx
+++ app/theme-provider.tsx
@@ -4,7 +4,13 @@ import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
   return (
-    <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      storageKey="theme"
+      disableTransitionOnChange={false}
+      enableColorScheme>
       {children}
     </NextThemesProvider>
   );
