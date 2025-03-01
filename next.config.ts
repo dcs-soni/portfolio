0882ff1b6dee96dc5f5771796f8f45be2a8d00fd
dcs-soni <52232900+dcs-soni@users.noreply.github.com>
2025-03-01 19:18:40 +0530

    fix: add images domain in next config for test

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
